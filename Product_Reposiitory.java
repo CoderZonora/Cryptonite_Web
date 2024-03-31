82430d537ac00d96d35a0a88822b7fb2fb5107bd
Rishabh <140229408+CoderZonora@users.noreply.github.com>
2024-03-31 17:23:04 +0530

    Update Product_Reposiitory.java

--- Product_Reposiitory.java
+++ Product_Reposiitory.java
@@ -5,7 +5,4 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface Product_Reposiitory extends CrudRepository<Product,Long> 
 //'Product'  is name of class managed by the repo and 'Long' is the data typr of its primary key.
-{
-    List<Product> findByName(String name);   // Have not implemented these, will have to try
-    List<Product> findByAuthor(String author);
-}
+{}
