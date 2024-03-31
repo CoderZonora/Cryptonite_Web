ef06e734c213aa33dd9ea405991c560bcd67ddaa
Rishabh <140229408+CoderZonora@users.noreply.github.com>
2024-03-31 17:31:02 +0530

    Update Product_Reposiitory.java

--- Product_Reposiitory.java
+++ Product_Reposiitory.java
@@ -4,5 +4,4 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface Product_Reposiitory extends CrudRepository<Product,Long> 
-//'Product'  is name of class managed by the repo and 'Long' is the data typr of its primary key.
 {}
