f65f3fd4d1dcb75981aefa70c115604194dc4ab9
Asus_Tuf <rishabhofficial2004@gmail.com>
2024-04-07 21:37:13 +0530

    Deleted old files

--- Product_Reposiitory.java
+++
@@ -1,7 +0,0 @@
-package com.example.CRUD;
-
-import java.util.List;
-import org.springframework.data.repository.CrudRepository;
-
-public interface Product_Reposiitory extends CrudRepository<Product,Long> 
-{}
